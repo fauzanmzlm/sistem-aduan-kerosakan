ece9428c70c28b74198f4b54982cbcffb3309856
fauzanmzlm <fauzanmazlam88@gmail.com>
2023-03-14 15:48:18 +0800

    update fields for create user

--- resources/views/admin/user/fields.blade.php
+++ resources/views/admin/user/fields.blade.php
@@ -8,13 +8,76 @@
         'required' => true,
     ])
 @else
-    @include('components.input', [
-        'type' => 'text',
-        'name' => 'username',
-        'label' => 'Username',
-        'required' => true,
-    ])
 
+    <div class="row">
+        <div class="col-6">
+
+        </div>
+        <div class="col-6">
+
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-6">
+            {{-- Username --}}
+            @include('components.input', [
+                'type' => 'text',
+                'name' => 'username',
+                'label' => 'Username',
+                'required' => true,
+            ])
+        </div>
+        <div class="col-6">
+            {{-- Identity Card Number --}}
+            @include('components.input', [
+                'type' => 'text',
+                'name' => 'identity_card_number',
+                'label' => 'Identity Card Number',
+                'required' => true,
+            ])
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-6">
+            {{-- Email --}}
+            @include('components.input', [
+                'type' => 'email',
+                'name' => 'email',
+                'label' => 'Email',
+                'required' => true,
+            ])
+        </div>
+        <div class="col-6">
+            {{-- Department/Unit --}}
+            @include('components.input', [
+                'type' => 'select',
+                'name' => 'department',
+                'label' => 'Department',
+                'required' => true,
+                'options' => $departmentOptions
+            ])
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-6">
+            {{-- First Name --}}
+            @include('components.input', [
+                'type' => 'text',
+                'name' => 'first_name',
+                'label' => 'First Name',
+                'required' => true,
+            ])
+        </div>
+        <div class="col-6">
+            {{-- Last Name --}}
+            @include('components.input', [
+                'type' => 'text',
+                'name' => 'last_name',
+                'label' => 'Last Name',
+                'required' => true,
+            ])
+        </div>
+    </div>
     <div class="form-group">
         <label>Role</label>
         <select class="select2 w-100" multiple="multiple">
