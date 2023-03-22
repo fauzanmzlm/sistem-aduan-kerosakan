4852113f342ee9e706ab89f004ffe64b2296ba5d
fauzanmzlm <fauzanmazlam88@gmail.com>
2023-03-22 15:26:06 +0800

    rename column name from name to full name

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -21,7 +21,7 @@
                                     <thead>
                                         <tr>
                                             {{-- <th>#</th> --}}
-                                            <th>Name</th>
+                                            <th>Full Name</th>
                                             <th>Username</th>
                                             <th>Email Address</th>
                                             <th>Role</th>
